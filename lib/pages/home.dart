0747e44e7b3c4f05284599fe82cf1eb042962409
Omar Faruk <omarfaruks2018@gmail.com>
2023-08-16 16:28:33 +0600

    update

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:shop_home_ui/models/category_models.dart';
+import 'package:shop_home_ui/models/diet_models.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
@@ -11,8 +12,18 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<CategoryModel> categories = [];
+  List<DietModel> diets = [];
 
-  void _getCategories(){
+  // void _getDiets(){
+  //   diets = DietModel.getDiets();
+  // }
+
+  void _getInitialInfo() {
+    categories = CategoryModel.getCategories();
+    diets = DietModel.getDiets();
+  }
+
+  void _getCategories() {
     categories = CategoryModel.getCategories();
   }
 
@@ -23,7 +34,7 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    _getCategories();
+    _getInitialInfo();
     return Scaffold(
       appBar: appBar(),
       backgroundColor: Colors.white,
@@ -32,56 +43,107 @@ class _HomePageState extends State<HomePage> {
         children: [
           _searchFiled(),
           const SizedBox(height: 40),
+          _categoriesSection(),
+          const SizedBox(height: 40),
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const Padding(
                 padding: EdgeInsets.only(left: 20),
                 child: Text(
-                  'Category',
+                  'Recommendation \nfor Diet',
                   style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 18,
-                    fontWeight: FontWeight.w600,
-                  ),
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600),
                 ),
               ),
               const SizedBox(height: 15),
               Container(
-                height: 120,
+                height: 240,
                 child: ListView.separated(
+                    itemBuilder: (context, index) {
+                      return Container(
+                        width: 210,
+                        decoration: BoxDecoration(
+                          color: diets[index].boxColor.withOpacity(0.3),
+                          borderRadius: BorderRadius.circular(20)
+                        ),
+                      );
+                    },
+                    separatorBuilder: (context, index) => SizedBox(width: 25,),
+                    itemCount: diets.length,
                   scrollDirection: Axis.horizontal,
-                  padding: const EdgeInsets.only(left: 20,right: 20),
-                  itemCount: categories.length,
-                  separatorBuilder: (context, index) =>const SizedBox(width: 25,),
-                  itemBuilder: (context, index) {
-                    return Container(
-                      width: 100,
-                      decoration: BoxDecoration(
-                        color: categories[index].boxColor.withOpacity(0.3)
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Container(
-                            width: 50,
-                            height: 50,
-                            decoration: const BoxDecoration(
-                              color: Colors.white,
-                              shape: BoxShape.circle
-                            ),
-                            child: SvgPicture.asset(categories[index].iconPath),
-                          ),
-                        ],
-                      ),
-                    );
-                  },
+                  padding: const EdgeInsets.only(
+                    left: 20,
+                    right: 20
+                  ),
                 ),
-              ),
+              )
             ],
-          ),
+          )
         ],
       ),
+    );
+  }
+
+  Column _categoriesSection() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Padding(
+          padding: EdgeInsets.only(left: 20),
+          child: Text(
+            'Category',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 18,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+        const SizedBox(height: 15),
+        Container(
+          height: 120,
+          child: ListView.separated(
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            itemCount: categories.length,
+            separatorBuilder: (context, index) => const SizedBox(
+              width: 25,
+            ),
+            itemBuilder: (context, index) {
+              return Container(
+                width: 100,
+                decoration: BoxDecoration(
+                    color: categories[index].boxColor.withOpacity(0.3)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      width: 50,
+                      height: 50,
+                      decoration: const BoxDecoration(
+                          color: Colors.white, shape: BoxShape.circle),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: SvgPicture.asset(categories[index].iconPath),
+                      ),
+                    ),
+                    Text(
+                      categories[index].name,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.w400,
+                          color: Colors.black,
+                          fontSize: 14),
+                    )
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 
